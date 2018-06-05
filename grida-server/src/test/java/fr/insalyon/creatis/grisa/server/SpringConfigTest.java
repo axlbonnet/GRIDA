d3ee9da3994373fc0cea9418c266a2ac33930616
Axel Bonnet <axel.bonnet@creatis.insa-lyon.fr>
2018-06-05 09:43:33 +0200

    add a functional test

--- grida-server/src/test/java/fr/insalyon/creatis/grisa/server/SpringConfigTest.java
+++ grida-server/src/test/java/fr/insalyon/creatis/grisa/server/SpringConfigTest.java
@@ -1,9 +1,18 @@
 package fr.insalyon.creatis.grisa.server;
 
+import fr.insalyon.creatis.grida.common.*;
+import fr.insalyon.creatis.grida.server.Constants;
 import fr.insalyon.creatis.grida.server.*;
+import fr.insalyon.creatis.grida.server.operation.Operations;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
+
+import java.io.IOException;
+import java.net.*;
+
+import static fr.insalyon.creatis.grida.common.Constants.MSG_SEP_1;
 
 /**
  * Created by abonnet on 5/17/18.
@@ -11,7 +20,7 @@ import org.springframework.context.annotation.*;
 public class SpringConfigTest {
 
     @Configuration
-    public static class SpringTestConfig {
+    public static class MostBasicTestConfig {
 
         @Bean
         public OperationTypeDetector operationTypeDetector(GridaConfiguration gridaConfiguration) {
@@ -25,9 +34,51 @@ public class SpringConfigTest {
 
     }
 
+    @Configuration
+    public static class MockableTestConfig {
+
+        @Bean
+        public OperationTypeDetector operationTypeDetector(GridaConfiguration gridaConfiguration) {
+            return new OperationTypeDetector(gridaConfiguration) {
+                @Override
+                public OperationType getOperationType() {
+                    return OperationType.LCG;
+                }
+            };
+        }
+
+
+        @Bean
+        public Operations operations() {
+            return Mockito.mock(Operations.class);
+        }
+
+    }
+
     @Test
     public void testSpringConfig() {
-        new AnnotationConfigApplicationContext(SpringConfiguration.class, SpringTestConfig.class);
+        new AnnotationConfigApplicationContext(SpringConfiguration.class, MostBasicTestConfig.class);
+    }
+
+    @Test
+    public void shouldCreateAFolder() throws IOException, InterruptedException {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(
+                SpringConfiguration.class,
+                MockableTestConfig.class);
+        Thread serverThread = new Thread( () -> ac.getBean(Server.class).run() );
+        serverThread.start();
+        Thread.sleep(2 * 1000);
+        Operations mockOperations = ac.getBean(Operations.class);
+        Communication communication = new Communication(new Socket(InetAddress.getLocalHost(), 9006));
+        communication.sendMessage(
+                ExecutorConstants.COM_CREATE_FOLDER + MSG_SEP_1
+                        + "fakeproxypath" + MSG_SEP_1
+                        + "/random/path");
+        communication.sendEndOfMessage();
+
+        System.out.println(communication.getMessage());
+        communication.close();
+
     }
 
 }
